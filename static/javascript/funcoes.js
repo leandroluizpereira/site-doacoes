c4bb2975ac908bfb86b19809e3f5f2e735a751f7
leandroluizpereira <leandro.2306@hotmail.com>
2022-04-10 12:24:43 -0300

    style

--- static/javascript/funcoes.js
+++ static/javascript/funcoes.js
@@ -2,4 +2,4 @@
 
 
 
-alert("tenho total autorização dos autores para o uso das imagens deste site,Criado por Leandro Pereira ");
+alert("tenho total autorização dos autores para o uso das imagens deste site");
